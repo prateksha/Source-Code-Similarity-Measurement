fb371b837917794d260a219a1ca09c46a5b15962
prateksha <pratekshau@gmail.com>
2020-10-25 20:13:57 +0530

    Reorganised folders

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@
 CWS=$(pwd)
 export PATH=$PATH:$(pwd)
 TEST_PROGRAM=$1
-REF_SOL=HackerRank/$TEST_PROGRAM/ref.py
-SUBMISSIONS=HackerRank/$TEST_PROGRAM/Correct
+REF_SOL=Datasets/$TEST_PROGRAM/ref.py
+SUBMISSIONS=Datasets/$TEST_PROGRAM/Correct
 python3 generate_ast.py 1 $REF_SOL
 for f in $SUBMISSIONS/*
 do
